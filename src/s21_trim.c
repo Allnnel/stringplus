c5ac9394745c530b27162602c790165d9f6f5716
Alana Enikeeva <120022626+Allnnel@users.noreply.github.com>
2024-02-11 14:55:44 +0300

    Update s21_trim.c

--- src/s21_trim.c
+++ src/s21_trim.c
@@ -7,22 +7,21 @@
 void *s21_trim(const char *src, const char *trim_chars) {
   char *result_str = s21_NULL;
 
-  if (src && trim_chars) {      // Если не пустые
-    int len = s21_strlen(src);  // Размер переданной строки
+  if (src && trim_chars) {     
+    int len = s21_strlen(src); 
     result_str =
-        (char *)calloc((len + 1), sizeof(char));  // Итоговая строка +1(\0)
-    int i = 0, j = 0;  // Переменные для циклов и хождению по символам строки
+        (char *)calloc((len + 1), sizeof(char)); 
+    int i = 0, j = 0; 
 
     for (; i < len && s21_strchr(trim_chars, src[i]); ++i)
-      ;  // Поиск начальных вхождений символов, как только strchr вернет null
-         // цикл закончится
-    for (; i < len; ++j)  // Цикл записи новой строки без первого вхожджения
-      result_str[j] = src[i++];  // Запись итоговой строки без символа с
-                                 // положения i из прошлого цикла
+      ;  
+    for (; i < len; ++j) 
+      result_str[j] = src[i++]; 
+                               
 
     for (; j != 0 && s21_strchr(trim_chars, result_str[--j]) &&
-           j < len;)  // Цикла поиска конечных вхождений символов
-      result_str[j] = '\0';  //Заменяем найденный символ на символ конца строки.
+           j < len;)  
+      result_str[j] = '\0'; 
   }
   return result_str;
 }
